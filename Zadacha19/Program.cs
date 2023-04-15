2cf978b8f73ced14ecf07a3099d59646f12ee698
forward_line <forward_line@mail.ru>
2023-04-15 22:10:24 +0500

    добавлено решение задачи 19 вариант преподавателя

--- Zadacha19/Program.cs
+++ Zadacha19/Program.cs
@@ -88,12 +88,66 @@ else
 
 
 
-
-/*{
-    Console.WriteLine($"{d} -> {array[2]}");
-}
-else
+/* Решение преподавателя
+int number = int.Parse(Console.ReadLine());
+bool result = CheckPolindrom(number);
+Console.WriteLine(result);
+bool CheckPolindrom(int number)
 {
-    Console.WriteLine("числа нет");
+    int[] array = ConvertToArray(number);
+    int[] reverseArray = ReverseArray(array);
+    bool result = Equals(array, reverseArray);
+    return result;
 }
-*/
+void PrintArray(int[] array)
+{
+    for (var i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+bool Equals(int[] array1, int[] array2)
+{
+    if (array1.Length != array2.Length)
+    {
+        return false;
+    }
+    for (var i = 0; i < array1.Length; i++)
+    {
+        if (array1[i] != array2[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int[] ReverseArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (var i = 0; i < array.Length; i++)
+    {
+        result[i] = array[array.Length - 1 - i];
+    }
+    return result;
+}
+int[] ConvertToArray(int number)
+{
+    int length = GetLengthNumber(number);
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+int GetLengthNumber(int number)
+{
+    int length = 0;
+    while (number != 0)
+    {
+        number = number / 10;
+        length++;
+    }
+    return length;
+}*/
